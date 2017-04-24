ac996e8cbacd0177bc3a2b46c32902085ecd3ac1
Lukasz Biedak <l.biedak@wp.pl>
2017-04-24 16:32:31 +0200

    Use loop in main

--- src/main/java/org/gen/algs/Main.java
+++ src/main/java/org/gen/algs/Main.java
@@ -1,7 +1,6 @@
 package org.gen.algs;
 
 import org.gen.algs.husband.PerfectHusband;
-import org.gen.algs.moto.Moto;
 import org.gen.algs.moto.ag.Population;
 
 public class Main {
@@ -10,23 +9,12 @@ public class Main {
     // TODO Auto-generated method stub
     Population p = new Population(new PerfectHusband());
     p.createPopulation();
-    System.out.println("0:" + p);
-    System.out.println("Worst:" + p.getWorst().toStringAsMappedObject());
-    
-    p.nextGeneration();
-    System.out.println("\n1:" + p);
-    System.out.println("Worst:" + p.getWorst().toStringAsMappedObject());
-    
-    p.nextGeneration();
-    System.out.println("\n2:" + p);
-    System.out.println("Worst:" + p.getWorst().toStringAsMappedObject());
-    p.nextGeneration();
-    System.out.println("\n3:" + p);
-    System.out.println("Worst:" + p.getWorst().toStringAsMappedObject());
-    p.nextGeneration();
-    System.out.println("\n4:" + p);
+    for (int i = 0; i < 5; ++i) {
+      System.out.println(i + ": " + p);
+      System.out.println("Worst: " + p.getWorst().toStringAsMappedObject());
+      p.nextGeneration();
+    }
 
-    
     System.out.println("Best:" + p.getBest().toStringAsMappedObject());
   }
 
